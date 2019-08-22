5d7379c999b367c44e406fdf14e2a60fcecb8689
tayir149 <53672170+tayir149@users.noreply.github.com>
2019-08-23 11:40:59 +1200

    Driver Interface: Header added CREATE TRIP button added SAVED TRIP button added EARNING Button added UPCOMING button added
    Signed-off-by: tayir149 <53672170+tayir149@users.noreply.github.com>

---
+++ app/src/main/java/com/example/uniride/DriverInterface.kt
@@ -0,0 +1,12 @@
+package com.example.uniride
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DriverInterface : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver_interface)
+    }
+}
