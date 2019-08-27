23e8e712b1c884df85bad4f859fbc4a97567fb32
JekkeyGrata <dqp6065@autuni.ac.nz>
2019-08-28 09:15:37 +1200

    adding functionality to tripsearcher added colors to colors.xml added tripsearch to manifest

--- app/src/main/java/com/example/uniride/TripSearcher.kt
+++ app/src/main/java/com/example/uniride/TripSearcher.kt
@@ -1,4 +1,18 @@
 package com.example.uniride
 
-class TripSearcher {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_tripsearcher.*
+
+class TripSearcher : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_trip)
+    }
 }
