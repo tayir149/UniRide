db10c1f1bba289b59db4138a28b5df31ca873752
timmehhhhh <vmm0807@autuni.ac.nz>
2019-08-28 09:59:59 +1200

    linked pages

--- app/src/main/java/com/example/uniride/SignUp.kt
+++ app/src/main/java/com/example/uniride/SignUp.kt
@@ -2,7 +2,10 @@ package com.example.uniride
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp: AppCompatActivity(){
@@ -11,10 +14,10 @@ class SignUp: AppCompatActivity(){
          super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_sign_up)
 
-        login.setOnClickListener{
-            val intent = Intent(this, PassengerDriverSelector:: class.java)
-            startActivity(intent)
+         login.setOnClickListener {
+             val intent = Intent(this, PassengerDriverSelector::class.java)
+             startActivity(intent)
+         }
         }
 
-    }
 }
