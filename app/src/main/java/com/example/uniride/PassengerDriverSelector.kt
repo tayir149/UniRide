4b69548bcee42e4f3c18b1ed98c2bdd8dd04974f
timmehhhhh <vmm0807@autuni.ac.nz>
2019-08-28 09:27:30 +1200

    sign up page links to passenger driver selector

---
+++ app/src/main/java/com/example/uniride/PassengerDriverSelector.kt
@@ -0,0 +1,13 @@
+package com.example.uniride
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PassengerDriverSelector: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passenger_driver_selector)
+    }
+}
