5dbd45705b46d52983c0bf969f939991bf1888d2
JasonLi3100 <paulamy3100@gmail.com>
2019-09-20 06:44:22 +1200

    history

--- app/src/main/java/com/example/uniride/activities/HistoryActivity.kt
+++ app/src/main/java/com/example/uniride/activities/HistoryActivity.kt
@@ -1,10 +1,23 @@
 package com.example.uniride.activities
 
+
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uniride.R
 import kotlinx.android.synthetic.main.activity_history.*
 import org.jetbrains.anko.startActivity
+
+
+
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -12,10 +25,17 @@ class HistoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
 
-        //TEST
-        button6.setOnClickListener {
-            val userID = "royalty37"
-            startActivity<Messenger>("userID" to userID)
+        val listView = findViewById<ListView>(R.id.history_listview)
+
+        //listView.adapter = MyCustomAdapter(this)
+
+        /* When HISTORY BACK button pressed, will navigate to last page */
+        history_back_button.setOnClickListener {
+            finish()
         }
+
+
+
     }
 }
+
