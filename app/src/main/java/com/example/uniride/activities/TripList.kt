08781c7ea89fc550766765367c64c9b97987d311
Jesselle <dqp6065@autuni.ac.nz>
2019-09-20 12:21:03 +1200

    testing changes

--- app/src/main/java/com/example/uniride/activities/TripList.kt
+++ app/src/main/java/com/example/uniride/activities/TripList.kt
@@ -2,6 +2,7 @@ package com.example.uniride.activities
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +11,18 @@ import android.widget.BaseAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uniride.R
 import kotlinx.android.synthetic.main.activity_triplist.*
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
+import java.sql.Time
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
 
 class TripList : AppCompatActivity() {
 
@@ -35,23 +42,30 @@ class TripList : AppCompatActivity() {
 
     private class MyCustomAdapter(context: Context):BaseAdapter(){
         private val mContext: Context = context
-        private val names = arrayListOf<String>(
-            "Name 1", "Name 2", "Name 3"
-        )
+
+        //testing
+        private val date = arrayListOf("2019-09-20","2019-09-21","2019-09-22")
+        private val pickupTime = arrayListOf("15:30","18:0","10:30")
+        private val etaValue = arrayListOf(30,20,30)
+        private val noPassengers = arrayListOf(0,2,1)
 
         override fun getItem(p0: Int): Any {return "trial"}
         override fun getItemId(p0: Int): Long {return p0.toLong()}
-        override fun getCount(): Int {return names.size}
+        override fun getCount(): Int {return date.size}
 
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
             val layoutInflater = LayoutInflater.from(mContext)
             val rowMain = layoutInflater.inflate(R.layout.activity_triprow, p2, false)
 
-            val driverTextView = rowMain.findViewById<TextView>(R.id.triplist_driver_textValue)
-            driverTextView.text = "Driver:"+names.get(p0)
-
+            val dateTextView = rowMain.findViewById<TextView>(R.id.triplist_date)
+            dateTextView.text = date[p0]
             val pickupTextView = rowMain.findViewById<TextView>(R.id.triplist_pickup_textValue)
-            pickupTextView.text = "Pickup Time: $p0"
+            pickupTextView.text = pickupTime[p0].toString()
+            val etaTextView = rowMain.findViewById<TextView>(R.id.triplist_eta_textValue)
+            etaTextView.text = "${etaValue[p0]} minutes"
+            val noPassengersTextView = rowMain.findViewById<TextView>(R.id.triplist_noPassengers_textValue)
+            noPassengersTextView.text = noPassengers[p0].toString()
 
             val messageDriverView = rowMain.findViewById<Button>(R.id.triplist_message_button)
             messageDriverView.setOnClickListener{
