12469f3794fa09b1ac164062eb450dd9be79a38a
JasonLi3100 <paulamy3100@gmail.com>
2019-10-07 18:48:20 +1300

    driver history

--- app/src/main/java/com/example/uniride/activities/DriverHistory.kt
+++ app/src/main/java/com/example/uniride/activities/DriverHistory.kt
@@ -3,11 +3,18 @@ package com.example.uniride.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.uniride.R
+import kotlinx.android.synthetic.main.activity_driver_history.*
+import org.jetbrains.anko.startActivity
 
 class DriverHistory : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver_history)
+
+        //when driver history button pressed, will switch to driver interface
+        driver_history_back_button.setOnClickListener {
+            startActivity<DriverInterface>()
+        }
     }
 }
