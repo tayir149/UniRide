83dc4734cedbf878246f59b0c93c9407ad0be8b4
timmehhhhh <vmm0807@autuni.ac.nz>
2019-10-09 08:46:21 +1300

    login works

--- app/src/main/java/com/example/uniride/activities/LogIn.kt
+++ app/src/main/java/com/example/uniride/activities/LogIn.kt
@@ -2,39 +2,52 @@ package com.example.uniride.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uniride.R
+import com.example.uniride.showToast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.login
 
 class LogIn: AppCompatActivity() {
 
+    var mAuth = FirebaseAuth.getInstance();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-         login.setOnClickListener {
-             val intent = Intent(this, PassengerDriverSelector::class.java)
-             startActivity(intent)
-         }
+        login.setOnClickListener {view ->
+            val email = university_email.text.toString();
+            val password = password_login.text.toString();
 
-//             val email = university_email.text.toString()
-//             val password = password_login.text.toString()
-//
-//             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-//        if (!it.
-//
-//         else if successful
-//        Log.d("Main","Successfully created user with uid: ${it.result.user.uid}")
-//        .addFailureListener
-//
-//
+            val intent = Intent(this, PassengerDriverSelector::class.java)
+            when {
+                email.isEmpty() -> showToast("Please enter your email!")
+                password.isEmpty() -> showToast("Please enter your password!")
+                else ->{
+                    signIn(email, password)
+                }
+            }
+        }
 
         signupnowbutton.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+    }
+    fun signIn(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener<AuthResult>{task ->
+            if(task.isSuccessful){
+                var intent = Intent(this, PassengerDriverSelector::class.java)
+                intent.putExtra("id", mAuth.currentUser?.email)
+                startActivity(intent)
+            }
+            else{
+                showToast("Error: ${task.exception?.message}")
+            }
+        })
     }
 }
