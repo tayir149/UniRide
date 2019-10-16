e33e49739468f7fe1002d9bd61b15a1fd17829c5
JasonLi3100 <paulamy3100@gmail.com>
2019-10-16 20:16:52 +1300

    saveTrip

--- app/src/main/java/com/example/uniride/activities/SaveTrips.kt
+++ app/src/main/java/com/example/uniride/activities/SaveTrips.kt
@@ -1,22 +1,34 @@
 package com.example.uniride.activities
 
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uniride.R
-import com.example.uniride.activities.SaveTrips.saveTripsAdapter.*
+//import com.example.uniride.activities.SaveTrips.saveTripsAdapter.*
 import com.example.uniride.classes.Trip
 import com.example.uniride.showToast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_save_trips.*
 import kotlinx.android.synthetic.main.activity_save_tripsrow.*
 import kotlinx.android.synthetic.main.activity_save_tripsrow.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SaveTrips : AppCompatActivity() {
 
@@ -25,7 +37,7 @@ class SaveTrips : AppCompatActivity() {
         setContentView(R.layout.activity_save_trips)
 
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-        val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email
+        //val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email
         val tripArray = ArrayList<Trip>()
         val uIdArray = ArrayList<String>()
 
@@ -34,7 +46,7 @@ class SaveTrips : AppCompatActivity() {
 
         //listView.adapter = adapter
 
-        val layoutManager = LinearLayoutManager(this)
+        /*val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         recyclerView.layoutManager = layoutManager
 
@@ -85,7 +97,7 @@ class SaveTrips : AppCompatActivity() {
             Log.d("Main", "get failed with", exception)
         }
 
-        save_trip_book_button.setOnClickListener {
+        saveTrips_back_button.setOnClickListener {
             finish()
         }
 
@@ -129,5 +141,115 @@ class SaveTrips : AppCompatActivity() {
             }
         }
 
+    }*/
+        val dateFormat = SimpleDateFormat("dd-mm-yyyy", Locale.getDefault())
+        val timeFormat = SimpleDateFormat("hh:mm", Locale.getDefault())
+
+        val listView = findViewById<ListView>(R.id.save_trip_listview)
+        val adapter = MyCustomAdapter(this, tripArray)
+
+        listView.adapter = adapter
+
+        val cal = Calendar.getInstance()
+        val nowDate = dateFormat.format(cal.time)
+        val nowTime = timeFormat.format(cal.time)
+
+
+        val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email
+        currentUserEmail?.let {
+            db.collection("users").document(it)
+        }
+            ?.get()?.addOnSuccessListener {
+                document ->
+                val savedTrips = document.get("booked_trips") as ArrayList<String>
+
+
+                db.collection("trips")
+                    .addSnapshotListener {
+                        value, e ->
+                        if (e != null) {
+                            Log.w("SaveTrips", "Listen failed.", e)
+                            return@addSnapshotListener
+                        }
+
+                        for (document in value!!.documentChanges) {
+                            if (document.type == DocumentChange.Type.ADDED) {
+                                val date = document.document.getString("date")
+                                val route = document.document.getString("route")
+                                val eta = document.document.getString("estimated_arrival_time")
+                                val details = document.document.getString("car_detail")
+                                val passengers =
+                                    document.document.getLong("number_of_passengers")?.toInt()
+                                val price = document.document.getDouble("price")
+                                val driverEmail = document.document.getString("user_email")
+                                val driverName = document.document.getString("trip_driver")
+                                val uId = document.document.id
+                                val passengerList =
+                                    document.document.get("passenger_list") as ArrayList<String>?
+
+                                val dateFormatted = dateFormat.parse(date)
+                                val currentDate = dateFormat.parse(nowDate)
+                                val timeFromDataBase = timeFormat.parse(eta)
+                                val currentTime = timeFormat.parse(nowTime)
+
+                                if (!savedTrips.contains((uId))) {
+                                    if ((dateFormatted.compareTo(currentDate) == 0 && timeFromDataBase <= currentTime)
+                                        || dateFormatted.compareTo(currentDate) < 0
+                                    ) {
+                                        tripArray.add(
+                                            Trip(
+                                                driverName,
+                                                date,
+                                                eta,
+                                                route,
+                                                price,
+                                                details,
+                                                passengers,
+                                                driverEmail,
+                                                passengerList
+                                            )
+                                        )
+                                        uIdArray.add(uId)
+                                    }
+                                }
+                            }
+
+                            adapter.notifyDataSetChanged()
+                        }
+                    }
+            }
+
+        saveTrips_back_button.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    private class MyCustomAdapter(context: Context, tripArray:ArrayList<Trip>):BaseAdapter() {
+        private val mContext: Context = context
+        private val array = tripArray
+
+
+
+
+        override fun getItem(p0: Int): Any {return "test"}
+        override fun getItemId(p0: Int): Long {return p0.toLong()}
+        override fun getCount(): Int {return array.size}
+
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.activity_save_tripsrow, p2, false)
+
+
+
+            val dateTextView = rowMain.findViewById<TextView>(R.id.save_trip_date)
+            dateTextView.text = array[p0].getDate()
+
+
+
+            return rowMain
+        }
     }
 }
